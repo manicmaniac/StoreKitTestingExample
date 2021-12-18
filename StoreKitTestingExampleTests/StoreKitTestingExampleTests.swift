5ae3a57c5b9ce1fe4329c385f7fcb97a65c6a4e1
Ryosuke Ito <rito.0305@gmail.com>
2021-12-18 22:24:14 +0900

    Add more detailed log

--- StoreKitTestingExampleTests/StoreKitTestingExampleTests.swift
+++ StoreKitTestingExampleTests/StoreKitTestingExampleTests.swift
@@ -5,10 +5,13 @@
 //  Created by Ryosuke Ito on 12/9/21.
 //
 
+import OSLog
 import StoreKit
 import StoreKitTest
 import XCTest
 @testable import StoreKitTestingExample
+
+private let logger = Logger(subsystem: "StoreKitTestingExampleTests", category: "StoreKitTestingExampleTests")
 
 class StoreKitTestingExampleTests: XCTestCase {
     private var session: SKTestSession!
@@ -16,6 +19,8 @@ class StoreKitTestingExampleTests: XCTestCase {
     override func setUpWithError() throws {
         session = try SKTestSession(configurationFileNamed: "Default")
         session.clearTransactions()
+        logger.debug("clear all transactions.")
+        // RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.25))
         session.resetToDefaultState()
         session.disableDialogs = true
     }
@@ -41,25 +46,26 @@ class StoreKitTestingExampleTests: XCTestCase {
         var transactionStates: [SKPaymentTransactionState] = []
         let paymentTransactionObserver = PaymentTransactionObserver { queue, transactions in
             dispatchPrecondition(condition: .onQueue(.main))
-            NSLog("--- Updating \(transactions.count) transactions")
+            logger.debug("updating \(transactions.count) transactions.")
             for transaction in transactions {
-                NSLog("--- Processing transaction '\(String(describing: transaction.transactionIdentifier))'")
+                logger.debug("processing \(transaction.transactionState) transaction with identifier \(String(describing: transaction.transactionIdentifier)).")
                 transactionStates.append(transaction.transactionState)
                 switch transaction.transactionState {
                 case .purchasing:
-                    NSLog("--- purchasing")
+                    break
                 case .purchased:
-                    NSLog("--- purchased")
                     queue.finishTransaction(transaction)
+                    logger.debug("finished transaction with identifier \(String(describing: transaction.transactionIdentifier)).")
                     expectation.fulfill()
                 case .failed:
-                    NSLog("--- failed")
+                    if let error = transaction.error {
+                        logger.error("transaction \(String(describing: transaction.transactionIdentifier)) failed with error \(error as NSError).")
+                    }
                     queue.finishTransaction(transaction)
+                    logger.debug("finished transaction with identifier \(String(describing: transaction.transactionIdentifier)).")
                 case .restored:
-                    NSLog("--- restored")
                     XCTFail("SKPaymentTransaction with restored state should not be received")
                 case .deferred:
-                    NSLog("--- deferred")
                     XCTFail("SKPaymentTransaction with deferred state should not be received")
                 @unknown default:
                     break
@@ -74,6 +80,7 @@ class StoreKitTestingExampleTests: XCTestCase {
         let payment = SKPayment(product: product)
         let paymentQueue = SKPaymentQueue.default()
         paymentQueue.add(paymentTransactionObserver)
+        logger.debug("added payment \(payment) to the payment queue.")
         paymentQueue.add(payment)
         waitForExpectations(timeout: 5)
         XCTAssertEqual(transactionStates, [.purchasing, .purchased])
@@ -170,5 +177,24 @@ private class ProductsRequestHandler: NSObject, SKProductsRequestDelegate {
 
     func request(_ request: SKRequest, didFailWithError error: Error) {
         failHandler?(request, error)
+    }
+}
+
+extension SKPaymentTransactionState: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .purchasing:
+            return "purchasing"
+        case .purchased:
+            return "purchased"
+        case .failed:
+            return "failed"
+        case .restored:
+            return "restored"
+        case .deferred:
+            return "deferred"
+        @unknown default:
+            return "unknown (\(rawValue)"
+        }
     }
 }
